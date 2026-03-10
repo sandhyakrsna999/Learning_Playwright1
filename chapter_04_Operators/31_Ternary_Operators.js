989fa92f40d6f5a901558acb5ffb2fd6f8b62949
Sandhya <sandhya.krsna999@gmail.com>
2026-03-10 15:13:08 +0530

    Committing all changes

---
+++ chapter_04_Operators/31_Ternary_Operators.js
@@ -0,0 +1,5 @@
+let age = 20;
+
+let is_sandhya_will_go_to_goa = age > 18 ? "Yes, let go goa!" : "No you are minor, Not going";
+console.log(is_sandhya_will_go_to_goa)
+// Ternary = condition ? "true result" : "false result";
