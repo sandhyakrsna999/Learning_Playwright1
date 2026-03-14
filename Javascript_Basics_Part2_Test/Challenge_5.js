9316cacadbff18614793b805a31a22d724920567
Sandhya <sandhya.krsna999@gmail.com>
2026-03-14 17:15:36 +0530

    Committing changes for Challenge_5.js

---
+++ Javascript_Basics_Part2_Test/Challenge_5.js
@@ -0,0 +1,15 @@
+let totalusers = 8;
+
+for (let a = 1; a <= totalusers; a++) {
+
+
+    if (a % 3 === 0) {
+        console.log("INACTIVE")
+    }
+    else {
+        console.log("ACTIVE");
+    }
+
+}
+
+//USR-0001 | TestUser_1 | testuser1@testingacademy.com | admin | ACTIVE
