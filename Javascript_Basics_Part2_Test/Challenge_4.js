39cdf3956477c508a5bfc174812937005fe8a362
Sandhya <sandhya.krsna999@gmail.com>
2026-03-14 16:53:17 +0530

    Committing changes for Challenge_4.js

---
+++ Javascript_Basics_Part2_Test/Challenge_4.js
@@ -0,0 +1,15 @@
+let isPresent = true;
+let isDisabled = true;
+let isEnabled = false;
+
+
+if (isPresent !== true) {
+    console.log("NOT FOUND");
+} else if (isPresent === true) {
+    console.log("HIDDEN");
+} else if (isEnabled !== true) {
+    console.log("DISABLED");
+} else {
+    console.log("READY");
+}
+
